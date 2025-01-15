e07ac2639c01f585ec7701d49dbc8eabef3eb5c5
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-15 15:38:37 +0200

    Update skill percentages and add new skills
    Updated the proficiency percentages for existing skills and added new skills to the skills list in the Skill model. Adjusted the percentages to reflect more accurate proficiency levels and included additional skills relevant to the project.

--- lib/features/landing_view/models/skill.dart
+++ lib/features/landing_view/models/skill.dart
@@ -8,17 +8,22 @@ class Skill {
   });
 
   static const List<Skill> skills = [
-    Skill(name: 'Dart', percent: 0.85),
+    Skill(name: 'Dart', percent: 0.9),
     Skill(name: 'OOP', percent: 0.8),
-    Skill(name: 'Flutter Framework', percent: 0.8),
+    Skill(name: 'Flutter Framework', percent: 0.95),
     Skill(name: 'Problem Solving', percent: 0.7),
-    Skill(name: 'Version Control (Git)', percent: 0.85),
-    Skill(name: 'UI/UX Implementation', percent: 0.8),
-    Skill(name: 'State Management (Cubit & Bloc)', percent: 0.9),
-    Skill(name: 'Firebase', percent: 0.8),
+    Skill(name: 'Version Control (Git)', percent: 0.94),
+    Skill(name: 'State Management (Cubit & Bloc)', percent: 0.95),
+    Skill(name: 'Firebase', percent: 0.9),
     Skill(name: 'APIs Handling', percent: 0.9),
-    Skill(name: 'Clean Architecture', percent: 0.8),
+    Skill(name: 'Clean Architecture', percent: 0.75),
     Skill(name: 'MVVM Pattern', percent: 0.9),
-    Skill(name: 'Responsiveness', percent: 0.8),
+    Skill(name: 'Responsive Design', percent: 0.9),
+    Skill(name: 'Flavors', percent: 0.8),
+    Skill(name: 'Third-Party Libraries', percent: 0.85),
+    Skill(name: 'CI/CD', percent: 0.9),
+    Skill(name: 'Design Patterns', percent: 0.8),
+    Skill(name: 'Testing', percent: 0.75),
+    Skill(name: 'Dependency Injection', percent: 0.8),
   ];
 }
