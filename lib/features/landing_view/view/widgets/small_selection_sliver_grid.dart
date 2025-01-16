9a267a6411753cbd37ae73fcc1d967e3e7ed0f49
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-16 12:27:31 +0200

    Update SmallSelectionSliverGrid layout for improved spacing and aspect ratio

--- lib/features/landing_view/view/widgets/small_selection_sliver_grid.dart
+++ lib/features/landing_view/view/widgets/small_selection_sliver_grid.dart
@@ -6,17 +6,14 @@ import 'package:personal_portfolio/features/landing_view/view/widgets/animated_p
 class SmallSelectionSliverGrid extends StatelessWidget {
   const SmallSelectionSliverGrid({
     super.key,
-    this.tabletLayoutChildAspectRatio,
   });
-
-  final double? tabletLayoutChildAspectRatio;
 
   @override
   Widget build(BuildContext context) {
     return SliverGrid.count(
       crossAxisCount: 2,
-      crossAxisSpacing: 40.w,
-      childAspectRatio: tabletLayoutChildAspectRatio ?? 1,
+      crossAxisSpacing: 24.w,
+      childAspectRatio: .85,
       children: List.generate(
         2,
         (index) => AnimatedProjectItem(
