e6ee267c67472e3446e67a56697ca8e841c411d3
HusseinMohamed99 <husseinhtm99@gmail.com>
2025-01-16 10:32:43 +0200

    Refactor build method in PersonalPortfolio for improved readability

--- lib/personal_portfolio.dart
+++ lib/personal_portfolio.dart
@@ -11,27 +11,30 @@ class PersonalPortfolio extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return ScreenUtilInit(
-        designSize: const Size(1440, 944),
-        minTextAdapt: true,
-        splitScreenMode: true,
-        builder: (_, child) {
-          SystemChrome.setPreferredOrientations([
+      designSize: const Size(1440, 944),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (_, child) {
+        SystemChrome.setPreferredOrientations(
+          [
             DeviceOrientation.portraitUp,
             DeviceOrientation.portraitDown,
-          ]);
-          return MediaQuery(
-            data: MediaQuery.of(context).copyWith(
-              textScaler: const TextScaler.linear(1.0),
-            ),
-            child: MaterialApp(
-              debugShowCheckedModeBanner: false,
-              title: 'Personal Portfolio',
-              theme: ThemeData.dark(),
-              themeMode: ThemeMode.dark,
-              onGenerateRoute: appRouter.generateRoute,
-              initialRoute: Routes.landingPageView,
-            ),
-          );
-        });
+          ],
+        );
+        return MediaQuery(
+          data: MediaQuery.of(context).copyWith(
+            textScaler: const TextScaler.linear(1.0),
+          ),
+          child: MaterialApp(
+            debugShowCheckedModeBanner: false,
+            title: 'Personal Portfolio',
+            theme: ThemeData.dark(),
+            themeMode: ThemeMode.dark,
+            onGenerateRoute: appRouter.generateRoute,
+            initialRoute: Routes.landingPageView,
+          ),
+        );
+      },
+    );
   }
 }
